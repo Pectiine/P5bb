7d0eca45fa15c77707db27a7242fc00bcf6c213b
Sssandra27 <62666165+Pectiine@users.noreply.github.com>
2020-08-21 12:02:45 +0200

    final2

--- migrations/Version20200821094352.php
+++ migrations/Version20200821094352.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200724123355 extends AbstractMigration
+final class Version20200821094352 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,12 @@ final class Version20200724123355 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property ADD slug VARCHAR(255) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDE989D9B62 ON property (slug)');
+        $this->addSql('ALTER TABLE property CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8BF21CDE989D9B62 ON property');
-        $this->addSql('ALTER TABLE property DROP slug');
+        $this->addSql('ALTER TABLE property CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
 }
